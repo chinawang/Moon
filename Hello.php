74807c892efda727fe8c405263b4bbfb42531a50
chinawang <chinawangyx@hotmail.com>
2016-11-18 15:58:17 +0800

    first commit

---
+++ Hello.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyx
+ * Date: 16/11/17
+ * Time: 17:01
+ */
+echo "Hello word!";
+
+echo "fldjfldj";
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Today is fuck day!";
+
+?>
